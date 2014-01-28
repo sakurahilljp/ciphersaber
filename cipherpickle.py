f837a5b00fd9accca611e958468bfdf681108054
snihonya <satoshi_nihonyanagi@agilent.com>
2014-01-28 14:24:50 +0900

    cipherpickle.py: cipherlib.py was merged into this.

--- cipherpickle.py
+++ cipherpickle.py
@@ -1,7 +1,6 @@
 #!/bin/env python
 
-import sys
-import os
+import StringIO
 import base64
 
 try:
@@ -9,13 +8,30 @@ try:
 except:
     import pickle
 
-import cipherlib
+import ciphersaber
+
+__N_DEFAULT = 20
+
+def encrypt(plain_text, key, n=__N_DEFAULT):
+    fin = StringIO.StringIO(plain_text)
+    fout = StringIO.StringIO()
+    
+    ciphersaber.process(True, fin, key, n, fout)
+    return base64.standard_b64encode(fout.getvalue())
+
+
+def decrypt(encrypted_text, key, n=__N_DEFAULT):
+    fin = StringIO.StringIO(base64.standard_b64decode(encrypted_text))
+    fout = StringIO.StringIO()
+
+    ciphersaber.process(False, fin, key, n, fout)
+    return fout.getvalue()
 
 def dumps(obj, key):
-    return cipherlib.encrypt(pickle.dumps(obj), key)
+    return encrypt(pickle.dumps(obj), key)
 
 def loads(encrypted, key):
-    return pickle.loads(cipherlib.decrypt(encrypted, key))
+    return pickle.loads(decrypt(encrypted, key))
 
 def test():
     obj = [[ i for i in range(50)],
@@ -30,6 +46,12 @@ def test():
     print obj
     print encrypted
     assert loads(encrypted,key) == obj
+
+    texts = [['Cipher Saver', '123456'],
+             ['Test Test Test', 'qwe'], ]
+
+    for text, key in texts:
+        assert decrypt(encrypt(text,key),key) == text, 'Failed to encrypt/decrypt.' 
 
 def main():
     test()
